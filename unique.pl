b7322dadae0539f31e0246b9ebf71ddf60c0ceb3
Matt Monsen <matt@mattmonsen.com>
2013-08-16 22:05:30 -0600

    Add List::MoreUtils to the benchmark

--- unique.pl
+++ unique.pl
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 
 use Benchmark;
+use List::MoreUtils qw(uniq);
 
 my @list;
 for ( 0..9999 ) {
@@ -16,6 +17,7 @@ timethese( 1000, {
     'keys_map_undef'  => sub { my @uniq = keys %{{ map {$_ => undef} @list }}; },
     'grep_seen_empty' => sub { my @uniq = do { my %seen; @seen{@list}=(); keys %seen; } }, 
     'grep_seen_undef' => sub { my @uniq = do { my %seen; undef @seen{@list}; keys %seen; } }, 
+    'list_more_utils' => sub { my @uniq = uniq @list; },
 });
 
 my @nums = (1, 3, 6, 3, 2, 5, 1, 7, 3, 6, 7, 8, 7, 9, 2);
