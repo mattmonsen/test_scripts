a7d1091b31e26850f022f0affa5ac2a3c8dee62a
Matt Monsen <matt@mattmonsen.com>
2013-12-04 22:07:29 -0700

    Fix a typo with the test names

--- unique.pl
+++ unique.pl
@@ -11,12 +11,12 @@ for ( 0..9999 ) {
     push @list, sprintf "%d", 100 * rand ;
 }
 
-timethese( 1000, {
+timethese( 10000, {
     'keys_map'        => sub { my @uniq = keys %{{ map {$_ => 1} @list }}; },
     'grep_seen'       => sub { my %seen; my @uniq = grep ! $seen{$_}++ , @list; },
     'keys_map_undef'  => sub { my @uniq = keys %{{ map {$_ => undef} @list }}; },
-    'grep_seen_empty' => sub { my @uniq = do { my %seen; @seen{@list}=(); keys %seen; } }, 
-    'grep_seen_undef' => sub { my @uniq = do { my %seen; undef @seen{@list}; keys %seen; } }, 
+    'keys_seen_empty' => sub { my @uniq = do { my %seen; @seen{@list}=(); keys %seen; } }, 
+    'keys_seen_undef' => sub { my @uniq = do { my %seen; undef @seen{@list}; keys %seen; } }, 
     'list_more_utils' => sub { my @uniq = uniq @list; },
 });
 
